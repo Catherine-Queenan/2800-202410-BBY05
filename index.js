d437bc553735547e47a12fad4e5ce118a51c71db
Catherine Queenan <catherinerqueenan@gmail.com>
2024-05-14 15:29:51 -0700

    Added todo message on index.js; Added a todo message to determine the user type and authorization on each page and load the navbar and footer accordingly.

--- index.js
+++ index.js
@@ -82,6 +82,9 @@ function adminAuthorization(req, res, next) {
 	}
 }
 
+// TODO: Add access to pages and create a check for the user type and authorization
+// status to determine what footer and navbar to display
+
 app.get('/', (req, res) => {
 	res.render('index');
 });
