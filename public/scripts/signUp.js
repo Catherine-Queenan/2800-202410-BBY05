27f99d4fdd8f300a14b8363060597b7b60380ae8
Eugenie Kim <eugenie.kim@outlook.com>
2024-05-13 23:18:39 -0700

    Added the password confirmation feature to client and business owner sign up pages; Submit button is disabled as long as the passwords do not match A little warning pops up to remind the user that the passwords don't match

--- public/scripts/signUp.js
+++ public/scripts/signUp.js
@@ -1,12 +1,19 @@
-function confirmPassword(){
-    let password = document.getElementById('passwordField').value;
-    let confirmPassword = document.getElementById('confirmPasswordField').value
-    console.log('made it');
-    if(password != confirmPassword){
-        alert("Pawsword doesn't match");
-        return false;
-    }
-    return true;
-}
+let submitButton = document.getElementById("signUpSubmit");
+let passwords = document.querySelectorAll(".passwordField");
 
-document.getElementById('confirmPasswordField').addEventListener('input', confrimPassword());
+passwords.forEach((passwordInput) => {
+    passwordInput.addEventListener("input", (event) =>{
+
+        let pass = document.getElementById("passwordField").value;
+        let confirmPass = document.getElementById("confirmPasswordField").value;
+
+        if(pass == confirmPass){
+            submitButton.disabled = false;
+            document.getElementById("passwordWarning").innerHTML = "";
+        } else {
+            submitButton.disabled = true;
+            document.getElementById("passwordWarning").innerHTML = "Passwords don't match!";
+        }
+
+   })
+});
