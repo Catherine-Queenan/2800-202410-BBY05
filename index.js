e17c559d01670eb824c7b7cc73315db6a58a0f26
Evin <evin4369@gmail.com>
2024-05-17 10:23:20 -0700

    Added comments and safe logical code for account deletion.

--- index.js
+++ index.js
@@ -824,22 +824,28 @@ app.post('/addingDog', upload.array('dogUpload', 6), async (req, res) => {
     await userdb.collection('dogs').insertOne(dog);
     res.redirect('/profile');
 });
-//-----------------------------------------------
 
-app.get('/accountConfirmation', (req, res) => {
+app.get('/accountDeletion', (req, res) => {
 	res.render('accountDeletion', {loggedIn: isValidSession(req), name: req.session.name , userType: req.session.userType});
 });
 
 app.post('/deleteAccount', async (req, res) => {
-	// console.log(req.session.email);
+
+	// Store the email
 	email = req.session.email;
-	
-	await userdb.dropDatabase();
+
+	// Logic for business accounts and clients (safe coding)
+	if (req.session.userType == 'client') {
+		await appUserCollection.deleteMany({email: email, userTupe: 'client'});
+		await userdb.dropDatabase();
+	} else if (req.session.userType == 'business') {
+		await appUserCollection.deleteMany({email: email, userTupe: 'business'});
+		await userdb.dropDatabase();
+	}
+
 	res.redirect('/logout');
-	// console.log(user);
-	
 });
-//-----------------------------------------------
+
 async function getUserEvents() {
 	var userEvents = await userdb.collection('eventSource').find().project({ title: 1, start: 1, end: 1, _id: 0 }).toArray();
 	// console.log(userEvents);
