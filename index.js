236450c79a61d010885006112c80a57375e4e6fa
kvnbanunu <kvnbanunu@gmail.com>
2024-05-22 12:18:18 -0700

    Added client-business connection for calendar; Added temporary code to link a client to a business-user; The business user can now select a client for session booking as well as adding text information. Styled the calendar to match our apps colour scheme.

--- index.js
+++ index.js
@@ -84,6 +84,7 @@ const appUserCollection = appdb.db(mongodb_appdb).collection('users');
 app.set('view engine', 'ejs');
 
 app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 // This is for storing active sessions
 var mongoStore = MongoStore.create({
@@ -200,7 +201,6 @@ async function setTrainerDatabase(req) {
 		trainerdb = null;
 	} else {
 		let trainer = await appUserCollection.find({ email: req.session.email}).project({companyName: 1, _id: 1}).toArray();
-		console.log(trainer);
 		// Checking for if the client currently has a hired trainer.
 		if (trainer[0].companyName == null || trainer[0].companyName == '' || trainer[0].companyName == undefined) {
 			trainerdb = null;
@@ -960,30 +960,42 @@ app.post('/deleteAccount', async (req, res) => {
 
 // ----------------- CALENDAR STUFF GOES HERE -------------------
 
+app.get('/findTrainer', async (req, res) => {
+	const trainers = await appUserCollection.find({ userType: 'business' }).project({ _id: 1, companyName: 1 }).toArray();
+	res.render('findTrainer', {loggedIn: isValidSession(req), userType: req.session.userType, trainers: trainers});
+});
+
+// Temporary add Trainer button for this Branch
+app.post('/addTrainer/:trainer', async (req, res) => {
+	let trainer = req.params.trainer;
+	await appUserCollection.updateOne({email: req.session.email}, {$set: { companyName: trainer}});
+
+	await setTrainerDatabase(req);
+
+	let client = await userdb.collection('info').find().project({email: 1, firstName: 1, lastName: 1, phone: 1}).toArray();
+
+	trainerdb.collection('clients').insertOne(client[0]);
+
+	res.redirect('/');
+});
+
 async function getUserEvents(req) {
 	let userEvents;
 	if (req.session.userType == 'business') {
-		userEvents = await userdb.collection('eventSource').find().project({ title: 1, start: 1, end: 1, _id: 0 }).toArray();
+		userEvents = await userdb.collection('eventSource').find().project({ _id: 1, title: 1, client: 1, start: 1, end: 1, info: 1}).toArray();
 	} else if (req.session.userType == 'client') {
 		if (trainerdb == null || trainerdb == '' || trainerdb == undefined) {
 			userEvents = null;
 		} else {
-			userEvents = await trainerdb.collection('eventSource').find().project({title: 1, start: 1, end: 1, _id: 0}).toArray();
+			userEvents = await trainerdb.collection('eventSource').find().project({ _id: 1, title: 1, client: 1, start: 1, end: 1, info: 1}).toArray();
 		}
 	}
-	console.log(userEvents);
 	return userEvents;
 }
 
-// Temporary add Trainer button for this Branch
-app.get('/addTrainer', async (req, res) => {
-	await appUserCollection.updateOne({email: req.session.email}, {$set: { companyName: 'Banunu'}});
-	res.redirect('/');
-});
-
 app.get('/calendar', async (req, res) => {
 	setUserDatabase(req);
-	setTrainerDatabase(req);
+	await setTrainerDatabase(req);
 	if (req.session.userType == 'business') {
 		res.render('calendarBusiness', {loggedIn: isValidSession(req), userType: req.session.userType});
 		return;
@@ -993,67 +1005,121 @@ app.get('/calendar', async (req, res) => {
 	
 });
 
+// Returns all events to the calendar
 app.get('/events', async (req, res) => {
 	const events = await getUserEvents(req);
 	res.json(events);
 });
 
+app.post('/getThisEvent', async (req, res) => {
+	let event = {
+		title: req.body.title,
+		start: req.body.start,
+		end: req.body.end
+	}
+	let result = await userdb.collection('eventSource').find(event).project({_id: 1, client: 1, info: 1}).toArray();
+	res.json(result);
+});
+
+// Returns client list to the calendar
+app.post('/getClients', async (req, res) => {
+	const clientList = await userdb.collection('clients').find().project({ email: 1, _id: 1 }).toArray();
+	res.json(clientList);
+});
+
 app.post('/addEvent', async (req, res) => {
-	var date = req.body.calModDate;
-	var startDate = date + "T" + req.body.calModStartHH + ":" + req.body.calModStartMM + ":00";
-	var endDate = date + "T" + req.body.calModEndHH + ":" + req.body.calModEndMM + ":00";
-	var event = {
+	let date = req.body.calModDate;
+	let startDate = date + "T" + req.body.calModStartHH + ":" + req.body.calModStartMM + ":00";
+	let endDate = date + "T" + req.body.calModEndHH + ":" + req.body.calModEndMM + ":00";
+	let clientEmail = req.body.calModClient;
+	let eventInfo = req.body.calModInfo;
+	let event = {
 		title: req.body.calModTitle,
 		start: startDate,
-		end: endDate
+		end: endDate,
+		client: clientEmail,
+		info: eventInfo
 	};
 
-	await userdb.collection('eventSource').insertOne({
-		title: event.title,
+	// Check for duplicate event
+	let check = userdb.collection('eventSource').find({
+		event: event.title,
 		start: event.start,
-		end: event.end
-	});
+		end: event.end,
+		client: event.client
+	}).project({_id: 1}).toArray(); //Check for everything but info
+
+	if (check.length > 0) {
+		// TODO DUPE ERROR EXIT HERE
+	} else {
+		await userdb.collection('eventSource').insertOne(event);
+	}
+
 	res.redirect('/calendar');
 });
 
 app.post('/updateEvent', async (req, res) => {
-	var date = req.body.calModDate;
-	var startNew = date + "T" + req.body.calModStartHH + ":" + req.body.calModStartMM + ":00";
-	var endNew = date + "T" + req.body.calModEndHH + ":" + req.body.calModEndMM + ":00";
-	var eventOrig = {
+	let date = req.body.calModDate;
+	let startNew = date + "T" + req.body.calModStartHH + ":" + req.body.calModStartMM + ":00";
+	let endNew = date + "T" + req.body.calModEndHH + ":" + req.body.calModEndMM + ":00";
+	let eventOrig = {
 		title: req.body.calModTitleOrig,
 		start: req.body.calModStartOrig,
-		end: req.body.calModEndOrig
+		end: req.body.calModEndOrig,
+		client: req.body.calModEmailOrig,
+		info: req.body.calModInfoOrig
 	}
 
-	var eventNew = {
+	let eventNew = {
 		title: req.body.calModTitle,
 		start: startNew,
-		end: endNew
+		end: endNew,
+		client: req.body.calModEmail,
+		info: req.body.calModInfo
 	}
 
 	await userdb.collection('eventSource').updateOne({
 		title: eventOrig.title,
 		start: eventOrig.start,
-		end: eventOrig.end
+		end: eventOrig.end,
+		client: eventOrig.client,
+		info: eventOrig.info
 	}, {
 		$set: {
 			title: eventNew.title,
 			start: eventNew.start,
-			end: eventNew.end
+			end: eventNew.end,
+			client: eventNew.client,
+			info: eventNew.info
 		}
 	});
+
+	// update using _id, but doesn't work
+	// let eventID = req.body.calModEventID;
+	// console.log(eventID);
+	// await userdb.collection('eventSource').updateOne({ _id: eventID }, { $set: {eventNew} });
+
 	res.redirect('/calendar');
 });
 
 app.post('/removeEvent', async (req, res) => {
-	var calTitle = req.body.calModTitleOrig;
-	var calStart = req.body.calModStartOrig;
-	var calEnd = req.body.calModEndOrig;
+
+	// Delete by _id, but doesn't work
+	// let eventID = req.body.calModEventID;
+	// await userdb.collection('eventSource').deleteOne({ _id: eventID});
+
+	let calTitle = req.body.calModTitleOrig;
+	let calStart = req.body.calModStartOrig;
+	let calEnd = req.body.calModEndOrig;
+	let calEmail = req.body.calModEmailOrig;
+	let calInfo = req.body.calModInfoOrig;
+
 	await userdb.collection('eventSource').deleteOne({
 		title: calTitle,
 		start: calStart,
-		end: calEnd
+		end: calEnd,
+		client: calEmail,
+		info: calInfo
 	});
 	res.redirect('/calendar');
 });
--- public/scripts/calendarBusiness.js
+++ public/scripts/calendarBusiness.js
@@ -16,53 +16,47 @@ document.addEventListener('DOMContentLoaded', function () {
 			modViewTitle.innerText = "Session booked for " + info.event.start.toLocaleDateString("en-CA");
 
 			// Original Event values
-			var modTitle = info.event.title;
-			var modStartOrig = info.event.start;
-			var modEndOrig = info.event.end;
+			let modTitle = info.event.title;
+			let modStartOrig = info.event.start;
+			let modEndOrig = info.event.end;
 
 			// Fields to edit
-			var modStartHH = info.event.start.toLocaleString([], {hour:"2-digit", hour12:false}).padStart(2, '0');
-			var modStartMM = info.event.start.toLocaleString([], {minute:"2-digit"}).padStart(2, '0');
-			var modEndHH = info.event.end.toLocaleString([], {hour:"2-digit", hour12:false}).padStart(2, '0');
-			var modEndMM = info.event.start.toLocaleString([], {minute:"2-digit"}).padStart(2, '0');
+			let modStartHH = info.event.start.toLocaleString([], {hour:"2-digit", hour12:false}).padStart(2, '0');
+			let modStartMM = info.event.start.toLocaleString([], {minute:"2-digit"}).padStart(2, '0');
+			let modEndHH = info.event.end.toLocaleString([], {hour:"2-digit", hour12:false}).padStart(2, '0');
+			let modEndMM = info.event.end.toLocaleString([], {minute:"2-digit"}).padStart(2, '0');
 
 			// This will fill the modal with the event info
-			var modalBody = `
-			<label for="calModTitle" class="form-label">Session Title</label>
-			<input type="text" class="form-control-plaintext d-none" name="calModDate" value="${info.event.start.toLocaleDateString("en-CA")}">
-			<input type="text" class="form-control-plaintext d-none" name="calModTitleOrig" value="${modTitle}">
-			<input type="text" class="form-control-plaintext d-none" name="calModStartOrig" value="${formatDate(modStartOrig)}">
-			<input type="text" class="form-control-plaintext d-none" name="calModEndOrig" value="${formatDate(modEndOrig)}">
-			<input type="text" class="form-control-plaintext" name="calModTitle" id="calModTitle" value="${modTitle}">
+			let modalBody = `
+			<div class="mb-3">
+				<label for="calModTitle" class="form-label">Session Title</label>
+				<input type="text" class="form-control-plaintext d-none" name="calModEventID" id="calModEventID">
+				<input type="text" class="form-control-plaintext d-none" name="calModDate" value="${info.event.start.toLocaleDateString("en-CA")}">
+				<input type="text" class="form-control-plaintext d-none" name="calModTitleOrig" id="calModTitleOrig" value="${modTitle}">
+				<input type="text" class="form-control-plaintext d-none" name="calModStartOrig" id="calModStartOrig" value="${formatDate(modStartOrig)}">
+				<input type="text" class="form-control-plaintext d-none" name="calModEndOrig" id="calModEndOrig" value="${formatDate(modEndOrig)}">
+				<input type="text" class="form-control-plaintext" name="calModTitle" id="calModTitle" value="${modTitle}">
+			</div>
+
+			<div class="mb-3">
+				<label for="calModEmail" class="form-label">Client</label>
+				<input type="text" class="form-control-plaintext d-none" name="calModEmailOrig" id="calModEmailOrig">
+				<select class="form-select" disabled name="calModEmail" id="calModEmail">
+					<option selected id="selectedClient"></option>
+				<select>
+			</div>
+
 			<div class="mb-3">
 				<label for="calModStart" class="form-label">Start Time</label>
 				<div class="input-group" id="calModStart">
 					<select class="form-select" disabled name="calModStartHH" id="calModStartHH">
 						<option selected>${modStartHH}</option>
-						<option value="00">0</option>
-						<option value="01">1</option>
-						<option value="02">2</option>
-						<option value="03">3</option>
-						<option value="04">4</option>
-						<option value="05">5</option>
-						<option value="06">6</option>
-						<option value="07">7</option>
-						<option value="08">8</option>
-						<option value="09">9</option>
-						<option value="10">10</option>
-						<option value="11">11</option>
-						<option value="12">12</option>
-						<option value="13">13</option>
-						<option value="14">14</option>
-						<option value="15">15</option>
-						<option value="16">16</option>
-						<option value="17">17</option>
-						<option value="18">18</option>
-						<option value="19">19</option>
-						<option value="20">20</option>
-						<option value="21">21</option>
-						<option value="22">22</option>
-						<option value="23">23</option>
+						<option value="00">0</option><option value="01">1</option><option value="02">2</option><option value="03">3</option>
+						<option value="04">4</option><option value="05">5</option><option value="06">6</option><option value="07">7</option>
+						<option value="08">8</option><option value="09">9</option><option value="10">10</option><option value="11">11</option>
+						<option value="12">12</option><option value="13">13</option><option value="14">14</option><option value="15">15</option>
+						<option value="16">16</option><option value="17">17</option><option value="18">18</option><option value="19">19</option>
+						<option value="20">20</option><option value="21">21</option><option value="22">22</option><option value="23">23</option>
 					</select>
 					<span class="input-group-text">:</span>
 					<select class="form-select" disabled name="calModStartMM" id="calModStartMM">
@@ -80,30 +74,12 @@ document.addEventListener('DOMContentLoaded', function () {
 				<div class="input-group" id="calModEnd">
 					<select class="form-select" disabled name="calModEndHH" id="calModEndHH">
 						<option selected>${modEndHH}</option>
-						<option value="00">0</option>
-						<option value="01">1</option>
-						<option value="02">2</option>
-						<option value="03">3</option>
-						<option value="04">4</option>
-						<option value="05">5</option>
-						<option value="06">6</option>
-						<option value="07">7</option>
-						<option value="08">8</option>
-						<option value="09">9</option>
-						<option value="10">10</option>
-						<option value="11">11</option>
-						<option value="12">12</option>
-						<option value="13">13</option>
-						<option value="14">14</option>
-						<option value="15">15</option>
-						<option value="16">16</option>
-						<option value="17">17</option>
-						<option value="18">18</option>
-						<option value="19">19</option>
-						<option value="20">20</option>
-						<option value="21">21</option>
-						<option value="22">22</option>
-						<option value="23">23</option>
+						<option value="00">0</option><option value="01">1</option><option value="02">2</option><option value="03">3</option>
+						<option value="04">4</option><option value="05">5</option><option value="06">6</option><option value="07">7</option>
+						<option value="08">8</option><option value="09">9</option><option value="10">10</option><option value="11">11</option>
+						<option value="12">12</option><option value="13">13</option><option value="14">14</option><option value="15">15</option>
+						<option value="16">16</option><option value="17">17</option><option value="18">18</option><option value="19">19</option>
+						<option value="20">20</option><option value="21">21</option><option value="22">22</option><option value="23">23</option>
 					</select>
 					<span class="input-group-text">:</span>
 					<select class="form-select" disabled name="calModEndMM" id="calModEndMM">
@@ -117,26 +93,60 @@ document.addEventListener('DOMContentLoaded', function () {
 				</div>
 			</div>
 
+			<div class="mb-3">
+				<label for="calModInfo" class="form-label">Info</label>
+				<div class="input-group">
+					<input type="text" class="form-control-plaintext d-none" name="calModInfoOrig" id="calModInfoOrig">
+					<textarea class="form-control-plaintext" name="calModInfo" id="calModInfo"></textarea>
+				</div>
+			</div>
+				
+			</div>
+
 			<div class="modal-footer">
-				<button type="button" class="btn btn-danger" data-bs-dismiss="modal">Cancel</button>
-				<button type="button" id="calModEdit" class="btn btn-secondary">Edit</button>
-				<button formaction="/removeEvent" id="deleteButton" disabled class="btn btn-danger">Delete</button>
-				<button formaction="/updateEvent" id="saveButton" disabled class="btn btn-success">Save changes</button>
+				<div class="row">
+					<button type="button" class="btn btn-danger mb-3" data-bs-dismiss="modal">Cancel</button>
+					<button type="button" id="calModEdit" class="btn btn-secondary mb-3">Edit</button>
+					<button formaction="/removeEvent" id="deleteButton" disabled class="btn btn-danger mb-3">Delete</button>
+					<button formaction="/updateEvent" id="saveButton" disabled class="btn btn-primary mb-3">Save changes</button>
+				</div>
+			</div>
 			`;
 
 			// Add the above HTML into the modal
 			modBod.innerHTML = modalBody;
-			modal.show();
+
+			async function getClients() {
+				try {
+					let res = await axios.post('/getClients');
+					let clients = res.data;
+					console.log("clients: " + clients);
+
+					let emails = document.getElementById('calModEmail');
+					if (clients && clients.length > 0) {
+						for (let i = 0; i < clients.length; i++) {
+							let clientItem = document.createElement('option');
+							clientItem.innerText = clients[i].email;
+							emails.appendChild(clientItem);
+						}
+					}
+				} catch (error) {
+					console.error(error);
+				}
+			}
+			getClients();
 
 			// Containing all above buttons and input fields in variables
-			var editButton = document.getElementById("calModEdit");
-			var deleteButton = document.getElementById("deleteButton");
-			var saveButton = document.getElementById("saveButton");
-			var calModTitleEdit = document.getElementById("calModTitle");
-			var calModStartHHEdit = document.getElementById("calModStartHH");
-			var calModStartMMEdit = document.getElementById("calModStartMM");
-			var calModEndHHEdit = document.getElementById("calModEndHH");
-			var calModEndMMEdit = document.getElementById("calModEndMM");
+			let editButton = document.getElementById("calModEdit");
+			let deleteButton = document.getElementById("deleteButton");
+			let saveButton = document.getElementById("saveButton");
+			let calModTitleEdit = document.getElementById("calModTitle");
+			let calModEmailEdit = document.getElementById("calModEmail");
+			let calModStartHHEdit = document.getElementById("calModStartHH");
+			let calModStartMMEdit = document.getElementById("calModStartMM");
+			let calModEndHHEdit = document.getElementById("calModEndHH");
+			let calModEndMMEdit = document.getElementById("calModEndMM");
+			let calModInfoEdit = document.getElementById("calModInfo");
 
 			editButton.addEventListener("click", onEditClick, false);
 
@@ -145,15 +155,70 @@ document.addEventListener('DOMContentLoaded', function () {
 				modal.show();
 				calModTitleEdit.classList.remove("form-control-plaintext");
 				calModTitleEdit.classList.add("form-control");
-
+				calModEmailEdit.removeAttribute("disabled");
 				calModStartHHEdit.removeAttribute("disabled");
 				calModStartMMEdit.removeAttribute("disabled");
 				calModEndHHEdit.removeAttribute("disabled");
 				calModEndMMEdit.removeAttribute("disabled");
+				calModInfoEdit.classList.remove("form-control-plaintext");
+				calModInfoEdit.classList.add("form-control");
 				deleteButton.removeAttribute("disabled");
 				saveButton.removeAttribute("disabled");
 			}
 
+			// Getting extra info from database
+			let titleOrig = document.getElementById('calModTitle').value;
+			let startOrig = document.getElementById('calModStartOrig').value;
+			let endOrig = document.getElementById('calModEndOrig').value;
+
+			console.log(titleOrig);
+			console.log(startOrig);
+			console.log(endOrig);
+
+			let thisEvent, eventID, eventEmail, eventInfo;
+			function getThisEvent() {
+
+				$.ajax({
+					url: '/getThisEvent',
+					type: 'POST',
+					contentType: 'application/json',
+					data: JSON.stringify({
+						title: titleOrig,
+						start: startOrig,
+						end: endOrig
+					}),
+					success: function(res) {
+						thisEvent = res;
+						console.log("This Event: ", thisEvent);
+						eventID = thisEvent[0]._id;
+						eventEmail = thisEvent[0].client;
+						eventInfo = thisEvent[0].info;
+						console.log('id', eventID);
+						console.log('email', eventEmail);
+						console.log('info', eventInfo);
+						// From database
+						let modEventID = document.getElementById('calModEventID');
+						let modEmailOrig = document.getElementById('calModEmailOrig');
+						let modEmail = document.getElementById('selectedClient');
+						let modInfoOrig = document.getElementById('calModInfoOrig');
+						let modInfo = document.getElementById('calModInfo');
+
+						modEventID.setAttribute('value', eventID);
+						modEmailOrig.setAttribute('value', eventEmail);
+						modEmail.setAttribute('value', eventEmail);
+						modEmail.innerText = eventEmail;
+						modInfoOrig.setAttribute('value', eventInfo);
+						modInfo.innerText = eventInfo;
+					},
+					error: function(xhr, status, error) {
+						console.error("Error: ", error);
+					}
+				});
+
+			}
+			getThisEvent();
+
+			modal.show();
 		},
 		dateClick: function(info) {
 
@@ -168,30 +233,12 @@ document.addEventListener('DOMContentLoaded', function () {
 				<label for="calModStart" class="form-label">Start Time</label>
 				<div class="input-group" id="calModStart">
 					<select class="form-select" name="calModStartHH">
-						<option selected>00</option>
-						<option value="01">1</option>
-						<option value="02">2</option>
-						<option value="03">3</option>
-						<option value="04">4</option>
-						<option value="05">5</option>
-						<option value="06">6</option>
-						<option value="07">7</option>
-						<option value="08">8</option>
-						<option value="09">9</option>
-						<option value="10">10</option>
-						<option value="11">11</option>
-						<option value="12">12</option>
-						<option value="13">13</option>
-						<option value="14">14</option>
-						<option value="15">15</option>
-						<option value="16">16</option>
-						<option value="17">17</option>
-						<option value="18">18</option>
-						<option value="19">19</option>
-						<option value="20">20</option>
-						<option value="21">21</option>
-						<option value="22">22</option>
-						<option value="23">23</option>
+						<option selected>00</option><option value="01">1</option><option value="02">2</option><option value="03">3</option>
+						<option value="04">4</option><option value="05">5</option><option value="06">6</option><option value="07">7</option>
+						<option value="08">8</option><option value="09">9</option><option value="10">10</option><option value="11">11</option>
+						<option value="12">12</option><option value="13">13</option><option value="14">14</option><option value="15">15</option>
+						<option value="16">16</option><option value="17">17</option><option value="18">18</option><option value="19">19</option>
+						<option value="20">20</option><option value="21">21</option><option value="22">22</option><option value="23">23</option>
 					</select>
 					<span class="input-group-text">:</span>
 					<select class="form-select" name="calModStartMM">
@@ -206,35 +253,17 @@ document.addEventListener('DOMContentLoaded', function () {
 
 			<div class="mb-3">
 				<label for="calModEnd" class="form-label">End Time</label>
-				<div class=input-group id="calModEnd">
-					<select class=form-select name="calModEndHH">
-						<option selected>00</option>
-						<option value="01">1</option>
-						<option value="02">2</option>
-						<option value="03">3</option>
-						<option value="04">4</option>
-						<option value="05">5</option>
-						<option value="06">6</option>
-						<option value="07">7</option>
-						<option value="08">8</option>
-						<option value="09">9</option>
-						<option value="10">10</option>
-						<option value="11">11</option>
-						<option value="12">12</option>
-						<option value="13">13</option>
-						<option value="14">14</option>
-						<option value="15">15</option>
-						<option value="16">16</option>
-						<option value="17">17</option>
-						<option value="18">18</option>
-						<option value="19">19</option>
-						<option value="20">20</option>
-						<option value="21">21</option>
-						<option value="22">22</option>
-						<option value="23">23</option>
+				<div class="input-group" id="calModEnd">
+					<select class="form-select" name="calModEndHH">
+						<option selected>00</option><option value="01">1</option><option value="02">2</option><option value="03">3</option>
+						<option value="04">4</option><option value="05">5</option><option value="06">6</option><option value="07">7</option>
+						<option value="08">8</option><option value="09">9</option><option value="10">10</option><option value="11">11</option>
+						<option value="12">12</option><option value="13">13</option><option value="14">14</option><option value="15">15</option>
+						<option value="16">16</option><option value="17">17</option><option value="18">18</option><option value="19">19</option>
+						<option value="20">20</option><option value="21">21</option><option value="22">22</option><option value="23">23</option>
 					</select>
 					<span class="input-group-text">:</span>
-					<select class=form-select name="calModEndMM">
+					<select class="form-select" name="calModEndMM">
 						<option selected>00</option>
 						<option value="15">15</option>
 						<option value="30">30</option>
@@ -244,13 +273,48 @@ document.addEventListener('DOMContentLoaded', function () {
 				</div>
 			</div>
 
+			<div class="mb-3">
+				<label for="calModClient" class="form-label">Client</label>
+				<div class="input-group">
+					<select class="form-select" id="calModClient" name="calModClient">
+					</select>
+				</div>
 			</div>
 
-			<div class="modal-footer">
+			<div class="mb-3">
+				<label for="calModInfo" class="form-label">Info</label>
+				<div class="input-group">
+					<textarea class="form-control" name="calModInfo" id="calModInfo"></textarea>
+				</div>
+			</div>
+
+			<div class="modal-footer align-items-center">
 				<button type="button" class="btn btn-danger" data-bs-dismiss="modal">Cancel</button>
-				<button formaction="/addEvent" class="btn btn-success">Add Session</button>
+				<button formaction="/addEvent" class="btn btn-primary">Add Session</button>
+			</div>
 			`;
 			modBod.innerHTML = modalBody;
+
+			async function getClients() {
+				try {
+					let res = await axios.post('/getClients');
+					let clients = res.data;
+					console.log("clients: " + clients);
+
+					let modClient = document.getElementById('calModClient');
+					if (clients && clients.length > 0) {
+						for (let i = 0; i < clients.length; i++) {
+							let clientItem = document.createElement('option');
+							clientItem.innerText = clients[i].email;
+							modClient.appendChild(clientItem);
+						}
+					}
+				} catch (error) {
+					console.error(error);
+				}
+			}
+			getClients();
+
 			modal.show();
 		}
 	});
