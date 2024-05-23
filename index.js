541d6f7b1532b3335dab3f912854d3a9ad5c986d
Eugenie Kim <eugenie.kim@outlook.com>
2024-05-23 10:57:42 -0700

    Added cancel buttons to all places that needed them as of 05/23; Added cancel button to all profile edits and to adding a new program Deamed it unneccessary on the addDog page as there is a back button that cancels the action

--- index.js
+++ index.js
@@ -383,7 +383,6 @@ app.post('/submitSignup/:type', async (req, res) => {
 				businessPhone: Joi.string().pattern(/^[0-9\s]*$/).length(10).required(),
 				firstName: Joi.string().pattern(/^[a-zA-Z\s]*$/).max(20).required(),
 				lastName: Joi.string().pattern(/^[a-zA-Z\s]*$/).max(20).required(),
-				companyWebsite: Joi.string().pattern(/^(https?:\/\/)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&//=]*)$/),
 				password: Joi.string().max(20).min(2).required()
 			}
 		);
@@ -395,7 +394,6 @@ app.post('/submitSignup/:type', async (req, res) => {
 			businessPhone: req.body.businessPhone,
 			firstName: req.body.firstName,
 			lastName: req.body.lastName,
-			companyWebsite: req.body.companyWebsite,
 			password: req.body.password
 		};
 
@@ -408,6 +406,8 @@ app.post('/submitSignup/:type', async (req, res) => {
 			res.send(doc);
 			return;
 		}
+
+		user.companyWebsite = req.body.companyWebsite;
 
 		//hashes password for storing
 		user.password  = await bcrypt.hash(user.password, saltRounds);
