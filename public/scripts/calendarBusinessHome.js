a319228b7de408e954e1568384242b5045653cb4
Catherine Queenan <catherinerqueenan@gmail.com>
2024-05-28 14:19:27 -0700

    Restyled calendars, chats, and alerts; Added some styling to the background and borders of calendars, as well as headers. Resized headers for chats and added a border. Styled alert pages with spacing and consistent fonts and layouts from similar pages. Changed the links in "Helpful Resources" on the client page to match what it moved to on the general homepage.

--- public/scripts/calendarBusinessHome.js
+++ public/scripts/calendarBusinessHome.js
@@ -1,6 +1,6 @@
 // Displays the previously hidden sessionList
 const sessionListShow = document.getElementById('sessionList');
-sessionListShow.classList.add('bg-light', 'py-3', 'rounded');
+sessionListShow.classList.add('whiteBackground', 'py-3', 'rounded');
 sessionListShow.classList.remove('d-none');
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -21,7 +21,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
 // Displays the previously hidden calendar
 const calendarShow = document.getElementById('calendar');
-calendarShow.classList.add('bg-light', 'py-3', 'rounded');
+calendarShow.classList.add('whiteBackground', 'py-3', 'rounded');
 calendarShow.classList.remove('d-none');
 
 document.addEventListener('DOMContentLoaded', function () {
